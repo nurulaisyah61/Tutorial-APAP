f9e1e3e105334334c41d46577ac2abe321d13c74
nurul.aisyah61 <aisynurul99@gmail.com>
2018-09-07 14:20:03 +0700

    commit Dosen.java di temporaryBranch

---
+++ Dosen.java
@@ -0,0 +1,25 @@
+public class Dosen{
+    private String name;
+    private String nid;
+
+    public Mahasiswa() {
+        this.name = "Nurul Aisyah";
+        this.nid = "1606889660";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNid(String nid) {
+        this.nid = nid;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getNid() {
+        return this.nid;
+    }
+}
